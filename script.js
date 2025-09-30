d48e1095dcca03fe82dd80a7539135b6d728d893
Jão Augusto <joao.pauliukevicius.palma@escola.pr.gov.br>
2025-09-30 07:40:33 -0300

    aula dia 30/09

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const avanca = document.querySelectorAll('.btn-proximo')
 avanca.forEach(button >{
     button.addEventListener('clicl',function(){
 const atual =
-    }
+   const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-    )
-}    
-
-)
+atual.classList.remove('ativo');
+document.getElementById(proximoPassomoPasso).classList.add('ativo');
+      })
+})
